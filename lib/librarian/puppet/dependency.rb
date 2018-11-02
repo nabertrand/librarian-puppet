69d668131cbeebefdbb871f511c1f7f602518bc7
Nick Bertrand <bert0214@umn.edu>
2018-11-02 13:34:16 -0500

    Specify depedenent module in depenencies

--- lib/librarian/puppet/dependency.rb
+++ lib/librarian/puppet/dependency.rb
@@ -5,11 +5,27 @@ module Librarian
 
       include Librarian::Puppet::Util
 
-      def initialize(name, requirement, source)
+      attr_accessor :parent
+      private :parent=
+
+      def initialize(name, requirement, source, parent = nil)
         # Issue #235 fail if forge source is not defined
         raise Error, "forge entry is not defined in Puppetfile" if source.instance_of?(Array) && source.empty?
 
-        super(normalize_name(name), requirement, source)
+        name = normalize_name(name)
+
+        assert_name_valid! name
+
+        self.name = name
+        self.requirement = Requirement.new(requirement)
+        self.source = source
+        self.parent = parent
+
+        @manifests = nil
+      end
+
+      def to_s
+        "#{name} (#{requirement}) <#{source}> (from #{parent.nil? ? '<nil>' : parent})"
       end
 
     end
--- lib/librarian/puppet/resolver.rb
+++ lib/librarian/puppet/resolver.rb
@@ -4,11 +4,23 @@ module Librarian
   module Puppet
     class Resolver < Librarian::Resolver
 
+      class Implementation < Librarian::Resolver::Implementation
+        def sourced_dependency_for(dependency)
+          return dependency if dependency.source
+
+          source = dependency_source_map[dependency.name] || default_source
+          dependency.class.new(dependency.name, dependency.requirement, source, dependency.parent)
+        end
+      end
+
+      def implementation(spec)
+        Implementation.new(self, spec, :cyclic => cyclic)
+      end
+
       def sort(manifests)
         manifests = manifests.values if Hash === manifests
         manifests.sort_by(&:name)
       end
-
     end
   end
 end
